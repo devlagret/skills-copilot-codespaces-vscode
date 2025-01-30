d2089cecbb4171f01657dbf3d5fd9819b874b845
devlagret <66945986+devlagret@users.noreply.github.com>
2025-01-30 06:21:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        {name: 'HTML', level: 5},
+        {name: 'CSS', level: 5},
+        {name: 'JavaScript', level: 5},
+        {name: 'Node.js', level: 4},
+        {name: 'Express', level: 4},
+        {name: 'MongoDB', level: 4},
+        {name: 'React', level: 4},
+        {name: 'Angular', level: 3}
+    ];
+}
